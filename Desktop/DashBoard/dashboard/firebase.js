1e103e1588a9bb652f3250431ee008517dcb17c1
GM7651 <gerimato123@gmail.com>
2024-10-17 22:18:13 -0400

    Dashbaord Draft #1

--- Desktop/DashBoard/dashboard/firebase.js
+++ Desktop/DashBoard/dashboard/firebase.js
@@ -1,15 +1,14 @@
-// C:\Users\gerim\Desktop\DashBoard\dashboard\firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';  // Import Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0Wp-DZHfE80k6E60Cxt9ziTT8TnxyVb8",
-  authDomain: "shinecity-form.firebaseapp.com",
-  projectId: "shinecity-form",
-  storageBucket: "shinecity-form.appspot.com",
-  messagingSenderId: "448829430335",
-  appId: "1:448829430335:web:9daf36d4d9d1ea5681b68a",
-  measurementId: "G-L33X470NCE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
